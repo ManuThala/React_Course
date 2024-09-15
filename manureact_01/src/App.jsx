2027f98b28abc963072882a462b912d1dc3aeca5
manu <manuthala7@gmail.com>
2024-09-15 10:23:57 +0530

    mannu hooks part commit

---
+++ manureact_01/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  // let counter = 15;
+
+  const [counter, setCounter] = useState(15);
+
+  function IncreaseCount() {
+    setCounter(counter + 1);
+  }
+
+  function decreaseCount() {
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>counter count {counter}</h1>
+      <button onClick={IncreaseCount}>Increase{counter}</button>{" "}
+      <button onClick={decreaseCount}>decrease{counter}</button>
+      <p>footer{counter}</p>
+    </>
+  );
+}
+
+export default App;
